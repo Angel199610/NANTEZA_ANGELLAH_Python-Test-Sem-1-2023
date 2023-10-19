40cda37d533eed0c5032d8e6623da353226350e0
Angel199610 <kwagalaangel27@gmail.com>
2023-10-19 09:56:20 +0300

    Python_Test_One

---
+++ Question_one.py
@@ -0,0 +1,12 @@
+#Rewrite the Variables in both Snake Case and Camel Snake
+#Student Marks
+"student_marks" 
+"studentMarks"
+
+#total_Average Mark
+"total_average_mark"
+"totalAverageMark"
+
+#Convert the variable age =22 to a float value, print its new data type
+age = 22
+print(float(age))
---
+++ Question_two.py
@@ -0,0 +1,21 @@
+#Function to find th eaverage of two numbers 
+def average_two (x, y):
+    x = 0
+    y = 5
+    average = (x + y)/2
+    return average
+
+
+#Test the program with 2 different numbers 
+# x = 5
+# y = 20
+# result = average_two(x, y)
+# print(f"The Average of x and Y is:",{result})
+
+
+#A program that asks the user to input 3 numbers and find minimum number
+list_1 = float (input("Enter Your first number: "))
+list_2 = float (input("Enter Your Second number: "))
+list_3 = float (input("Enter Your third number: "))
+minimum_number = (list_1, list_2, list_3)
+print(minimum_number)
