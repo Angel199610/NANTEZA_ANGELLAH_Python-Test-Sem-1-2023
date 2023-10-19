b77b80b77aa9c55588f98305589bd5349b0fc526
Nanteza Angellah <142222757+Angel199610@users.noreply.github.com>
2023-10-19 10:16:42 +0300

    Python_Test_One

---
+++ python_test_one/Question_three.py
@@ -0,0 +1,23 @@
+# #A python program to sum all the items in the list 
+student_marks = [78, 83, 90,88, 75]
+student_marks = [78, 83, 90,88, 75]
+print(type(student_marks))
+print(f"The sum of the items in the student marks list is:")
+print(sum([78, 83, 90,88, 75]))
+
+# # #Display the first and last Mark
+student_marks = [78, 83, 90,88, 75]
+print(student_marks [0])
+print(student_marks [-1])
+
+
+# #Addinng a number to the list
+student_marks = [78, 83, 90,88, 75]
+student_marks.append(96)
+print(student_marks)
+
+
+#Update the student mark
+student_marks = [78, 83, 90,88, 75]
+student_marks [0] = 87
+print(student_marks)
