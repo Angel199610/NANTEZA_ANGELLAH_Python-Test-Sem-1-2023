4cee6c1eb0d8c729f8cb9c1b29e0a97844fd3daa
Angel199610 <kwagalaangel27@gmail.com>
2023-10-19 10:02:49 +0300

    Python_Test_One

--- Question_two.py
+++ Question_two.py
@@ -7,15 +7,15 @@ def average_two (x, y):
 
 
 #Test the program with 2 different numbers 
-# x = 5
-# y = 20
-# result = average_two(x, y)
-# print(f"The Average of x and Y is:",{result})
+x = 5
+y = 20
+result = average_two(x, y)
+print(f"The Average of x and Y is:",{result})
 
 
 #A program that asks the user to input 3 numbers and find minimum number
 list_1 = float (input("Enter Your first number: "))
 list_2 = float (input("Enter Your Second number: "))
 list_3 = float (input("Enter Your third number: "))
-minimum_number = (list_1, list_2, list_3)
-print(minimum_number)
+minimum_number = min(list_1, list_2, list_3)
+print(f"The Minimum number of the test is: ", {minimum_number})
